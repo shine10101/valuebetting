10287f962f7a428410ffab535f6f92ae566ce7ae
shine10101 <pshine147@gmail.com>
2023-01-08 15:17:42 +0000

    got to calculating team strength

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import datetime
 data = data_ingestion.get_links()
 
 for league in data[0]:
-    print(league['Div'][0])
     league = data[0]
     unique_dates = pd.DataFrame(league.Date.dt.strftime('%y-%m-%d').unique())
     unique_dates = [pd.to_datetime(t, format='%y-%m-%d') for t in unique_dates[0]]
     y = unique_dates[0] + datetime.timedelta(days=30)
     train_test_dates = [t for t in unique_dates if t > y]
     teams = pd.DataFrame(sorted(league.HomeTeam.unique()))
-    for day in train_test_dates:
+    for day in train_test_dates[0]:
         day = train_test_dates[0]
         Train = league[(league['Date'] < day)]
         Test = league[(league['Date'] == day)]
--- value_fb/poissonanalysis.py
+++ value_fb/poissonanalysis.py
@@ -3,27 +3,39 @@ import pandas as pd
 
 def analysis(Train, test, teams):
 
-    fix_train = Train[['HomeTeam', 'AwayTeam']]
-    home_form = []
-    away_form = []
+    home_form=[]
+    away_form=[]
     # calculate home and away strengths
     for team in teams[0]:
-        team = teams[0][0]
-        team_games = Train[(Train['HomeTeam']==team) | (Train['AwayTeam']==team)]
-        HF = dict()
-        AF = dict()
+        HF = []
+        AF = []
 
-        HF['GamesplayedH'] = len(Train[Train['HomeTeam']==team])
-        HF['GoalsforH'] = sum(Train['FTHG'][Train['HomeTeam']==team])
-        HF['AvggoalsforH'] = Train['FTHG'][Train['HomeTeam']==team].mean()
-        HF['GoalsagainstH'] = sum(Train['FTAG'][Train['HomeTeam']==team])
-        HF['AvggoalsagainstH'] = Train['FTAG'][Train['HomeTeam']==team].mean()
+        HF.append(len(Train[Train['HomeTeam']==team]))
+        HF.append(sum(Train['FTHG'][Train['HomeTeam']==team]))
+        HF.append(Train['FTHG'][Train['HomeTeam']==team].mean())
+        HF.append(sum(Train['FTAG'][Train['HomeTeam']==team]))
+        HF.append(Train['FTAG'][Train['HomeTeam']==team].mean())
 
-        AF['GamesplayedA'] = len(Train[Train['AwayTeam']==team])
-        AF['GoalsforA'] = sum(Train['FTAG'][Train['AwayTeam']==team])
-        AF['AvggoalsforA'] = Train['FTAG'][Train['AwayTeam']==team].mean()
-        AF['GoalsagainstA'] = sum(Train['FTHG'][Train['AwayTeam']==team])
-        AF['AvggoalsagainstA'] = Train['FTHG'][Train['AwayTeam']==team].mean()
+        AF.append(len(Train[Train['AwayTeam']==team]))
+        AF.append(sum(Train['FTAG'][Train['AwayTeam']==team]))
+        AF.append(Train['FTAG'][Train['AwayTeam']==team].mean())
+        AF.append(sum(Train['FTHG'][Train['AwayTeam']==team]))
+        AF.append(Train['FTHG'][Train['AwayTeam']==team].mean())
 
         home_form.append(HF)
         away_form.append(AF)
+
+    home_form = pd.DataFrame(home_form, columns=['GamesplayedH', 'GoalsforH', 'AvggoalsforH', 'GoalsagainstH', 'AvggoalsagainstH'])
+    away_form = pd.DataFrame(away_form, columns=['GamesplayedA', 'GoalsforA', 'AvggoalsforA', 'GoalsagainstA', 'AvggoalsagainstA'])
+
+    median_avg_H = home_form.median()
+    median_avg_A = away_form.median()
+
+    HomeAtkStr = home_form['AvggoalsforH'] / median_avg_H['AvggoalsforH']
+    HomeDefStr = home_form['AvggoalsagainstH'] / median_avg_H['AvggoalsagainstH']
+    AwayAtkStr = away_form['AvggoalsforA'] / median_avg_A['AvggoalsforA']
+    AwayDefStr = away_form['AvggoalsagainstA'] / median_avg_A['AvggoalsagainstA']
+
+    team_strength = pd.concat([HomeAtkStr, HomeDefStr, AwayAtkStr, AwayDefStr], axis = 1)
+
+    return team_strength
